78ed57ba57bf634cccb61c37f640340b6c5ab32b
KevivJaknap <vivekpankaj1@gmail.com>
2023-02-28 11:07:33 +0530

    centered

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -4,20 +4,31 @@
 	let password = '';
 </script>
 
-<div class="">
-	<div class="flex flex-col items-center justify-center">
-		<div class="my-2 mx-3 w-1/2 border-2 border-black ">
+<div class="relative">
+	<p class="absolute">Computer Security Chat App</p>
+	<div class="h-screen flex flex-col items-center justify-center">
+		<div class="min-w-min max-w-sm my-2 mx-3 w-1/2 border-2 border-black ">
 			<div class="mx-2">
 				<div class="">
 					<label for="username">Username</label>
-					<div class="border-2 border-black">
-						<input type="text" id="username" bind:value={username} />
+					<div class="my-2">
+						<input
+							class="rounded-md border-black border-2"
+							type="text"
+							id="username"
+							bind:value={username}
+						/>
 					</div>
 				</div>
-				<div class="mb-2">
+				<div class="my-4">
 					<label for="password">Password</label>
-					<div class="border-2 border-black">
-						<input type="password" id="password" bind:value={password} />
+					<div>
+						<input
+							class="w-auto rounded-md border-black border-2"
+							type="password"
+							id="password"
+							bind:value={password}
+						/>
 					</div>
 				</div>
 			</div>
